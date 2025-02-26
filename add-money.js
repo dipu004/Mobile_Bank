511679a48c77bc7f35e2006014d71ef386325e02
Md <dipum0505@gmail.com>
2025-02-26 14:08:32 +0600

    added Transactions section

--- add-money.js
+++ add-money.js
@@ -7,9 +7,22 @@ document.getElementById('add-money').addEventListener('click', function(event){
      const convertedPin = parseInt(Pin);
      const mainBalance = document.getElementById("main-balance").innerText;
      const contertedMainbalance = parseFloat(mainBalance);
+     const selectBank = document.getElementById("allbank").value;
+     console.log(selectBank);
      if(convertedPin === 1234){
         const sum = contertedMainbalance + convertedAmount;
         document.getElementById("main-balance").innerText = sum;
+
+const container = document.getElementById("Transactions-history");
+
+       const p = document.createElement("p");
+       p.innerText = `
+       added ${convertedAmount} from ${contertedMainbalance} Amount
+       ` 
+          console.log(p);
+
+         container.appendChild(p);
+
      }
      else{
         alert("pin sothik na");
--- script/cashout.js
+++ script/cashout.js
@@ -7,10 +7,25 @@ document.getElementById("cashout-btn").addEventListener('click' , function(event
         convertedCashout = parseFloat(cashoutAmount);
         const mainBalance =  document.getElementById("main-balance").innerText;
         const convertedMainbalance = parseFloat(mainBalance);
-
+        const selectBank = document.getElementById("allbank").value;
+        console.log(selectBank);
+             if(convertedCashout > convertedMainbalance){
+                alert("Invalid Amount")
+                return;
+             }
         if(convertedPin === 1234){
                 const sum = convertedMainbalance - convertedCashout;
                 document.getElementById("main-balance").innerText = sum;
+
+                const container = document.getElementById("Transactions-history");
+
+            const p = document.createElement("p");
+
+          p.innerText = `
+          cashout ${convertedCashout} form this ${convertedMainbalance}
+          `
+         container.appendChild(p)
+
         }
         else{
          alert("enter a valid pin")
